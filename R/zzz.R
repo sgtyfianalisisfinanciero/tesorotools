f2fb52121a904c2c0ee40aa322edf2ace16a4944
Fabián Salazar, Miguel <mfabian@economia.gob.es>
2023-08-17 15:30:41 +0200

    20230817 generar_grafico_basico() añadido

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(...) {
-  packageStartupMessage("tesorotools 20230814 - miguel@fabiansalazar.es")
+  packageStartupMessage("tesorotools 20230817 - miguel@fabiansalazar.es")
 
   grDevices::windowsFonts(Calibri = windowsFont("Calibri"))
 
